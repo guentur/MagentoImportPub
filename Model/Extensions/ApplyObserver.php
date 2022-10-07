e69ca34714cead7203eb8c733809a2c611861436
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-10-07 13:15:31 +0300

    refactor(model): Remove dependency from EntityScopeManager in ApplyObserver class. Add entity type to the event name

--- Model/Extensions/ApplyObserver.php
+++ Model/Extensions/ApplyObserver.php
@@ -5,30 +5,21 @@ namespace Guentur\MagentoImport\Model\Extensions;
 use Guentur\MagentoImport\Api\Extensions\ApplyObserverInterface;
 use Magento\Framework\DataObject;
 use Guentur\MagentoImport\Api\Data\DataImportInfoInterface;
-use Guentur\MagentoImport\Model\EntityScopeManager;
 use Magento\Framework\Event\ManagerInterface;
 
 class ApplyObserver implements ApplyObserverInterface
 {
-    /**
-     * @var EntityScopeManager
-     */
-    private $entityScopeManager;
-
     /**
      * @var ManagerInterface
      */
     private $eventManager;
 
     /**
-     * @param EntityScopeManager $entityScopeManager
      * @param ManagerInterface $eventManager
      */
     public function __construct(
-        EntityScopeManager $entityScopeManager,
         ManagerInterface $eventManager
     ) {
-        $this->entityScopeManager = $entityScopeManager;
         $this->eventManager = $eventManager;
     }
 
@@ -56,10 +47,16 @@ class ApplyObserver implements ApplyObserverInterface
      */
     public function getEventName(DataImportInfoInterface $dataImportInfo): string
     {
+        //@todo write in documentation that file name (not path, but exactly filename) of separate dataProviders must be different
+        $providerName = $dataImportInfo->getDataProviderName();
+        $recipientName = $dataImportInfo->getRecipientName();
+        $name = $providerName . '_' . $recipientName;
+
+        $providerType = $dataImportInfo->getDataProviderType();
+        $recipientType = $dataImportInfo->getRecipientType();
+        $type = $providerType . '_' . $recipientType;
+
         //@todo add importer type part to event name
-        return 'guentur_import_'
-            . $this->entityScopeManager->getEntityScopeEventFormat(
-                $dataImportInfo
-            );
+        return 'guentur_import_' . $name . '_' . $type;
     }
 }
