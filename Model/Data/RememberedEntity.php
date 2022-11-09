63bca28568731121b7b115e977d7f223fed05701
Kyrylo Peretiatko <kyrylo.peret@gmail.com>
2022-11-09 16:14:05 +0200

    feature(model): Add methods for getting data from rememberedEntity data-model

--- Model/Data/RememberedEntity.php
+++ Model/Data/RememberedEntity.php
@@ -6,6 +6,7 @@ use Guentur\MagentoImport\Api\Data\DataImportInfoInterface;
 use Magento\Framework\Api\AbstractSimpleObject;
 use Guentur\MagentoImport\Api\Data\RememberedEntityInterface;
 use Guentur\MagentoImport\Model\ResourceModel\RememberedEntity as RememberedEntityResource;
+use Magento\Framework\Api\AbstractExtensibleObject;
 
 class RememberedEntity extends AbstractSimpleObject implements RememberedEntityInterface
 {
@@ -25,6 +26,60 @@ class RememberedEntity extends AbstractSimpleObject implements RememberedEntityI
         return $this->_get($idFieldName);
     }
 
+    public function setScope(string $scope)
+    {
+        $this->setData('scope', $scope);
+    }
+
+    public function getScope()
+    {
+        return $this->_get('scope');
+    }
+
+    public function setRememberMode(string $rememberMode)
+    {
+        $this->setData('remember_mode', $rememberMode);
+    }
+
+    public function getRememberMode()
+    {
+        return $this->_get('remember_mode');
+    }
+
+    public function setRememberedEntityKey($rememberedEntityKey)
+    {
+        $this->setData('remembered_entity_key', $rememberedEntityKey);
+    }
+
+    /**
+     * @return mixed|null
+     */
+    public function getRememberedEntityKey()
+    {
+        return $this->_get('remembered_entity_key');
+    }
+
+    public function getCreatedAt()
+    {
+        return $this->_get('created_at');
+    }
+
+    public function getUpdatedAt()
+    {
+        return $this->_get('updated_at');
+    }
+
+    public function setCreatedAt($createdAt)
+    {
+        $this->setData('created_at', $createdAt);
+    }
+
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->setData('updated_at', $updatedAt);
+    }
+
+/////////////// Additional logic
     public function setPathToRecipient(string $pathToRecipient): void
     {
         $this->setData('path_to_recipient', $pathToRecipient);
@@ -57,14 +112,5 @@ class RememberedEntity extends AbstractSimpleObject implements RememberedEntityI
         return $pathToRecipient == null ? null : pathinfo($pathToRecipient)['filename'];
     }
 
-    public function setScope(string $scope)
-    {
-        $this->setData('scope', $scope);
-    }
-
-    public function getScope()
-    {
-        return $this->_get('scope');
-    }
-
+//@todo extend Magento\Framework\Api\AbstractExtensibleObject to give capability for third party developers to use extension attributes
 }
